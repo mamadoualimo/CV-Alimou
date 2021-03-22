9143daf9445b52aacc758c93d4b2fb20c05b0edc
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2021-03-22 17:28:57 +0100

    Route, navigation et styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Knowledges from './pages/Knowledges';
+import NotFound from './pages/NotFound';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    <div>
-      <h1>bonjour tout le monde</h1>
-    </div>
+    <>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/competences" exact component={Knowledges} />
+        <Route path="/portfolio" exact component={Portfolio} />
+        <Route path="/contact" exact component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+    </>
   );
 };
 
