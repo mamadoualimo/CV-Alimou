465b0d9e9b285bcbf729e5609d0c178af38030fa
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2021-03-22 18:02:43 +0100

    mise en place de la page d'accueil

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Navigation from '../components/Navigation';
 
 const NotFound = () => {
     return (
-        <div>
-            <Navigation />
+        <div className="notFound">
+            <div className="notFoundContent">
+                <h3>Désolé cette page n'existe pas !</h3>
+                <NavLink exact to="/">
+                    <i className="fas fa-home"></i>
+                    <span> Accueil </span>
+                </NavLink>
+            </div>
         </div>
     );
 };
